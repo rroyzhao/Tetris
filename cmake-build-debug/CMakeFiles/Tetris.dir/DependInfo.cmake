4f37fcf52d1cb159ded5eb55364c67a8c11a6942
ruiruizhao <rroy.zhao@gmail.com>
2020-05-27 20:49:03 +0800

    200527

---
+++ cmake-build-debug/CMakeFiles/Tetris.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Progect/C++/Tetris/Source/Block.cpp" "D:/Progect/C++/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/Source/Block.cpp.obj"
+  "D:/Progect/C++/Tetris/Source/Blocks.cpp" "D:/Progect/C++/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/Source/Blocks.cpp.obj"
+  "D:/Progect/C++/Tetris/Source/Board.cpp" "D:/Progect/C++/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/Source/Board.cpp.obj"
+  "D:/Progect/C++/Tetris/Source/OBlocks.cpp" "D:/Progect/C++/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/Source/OBlocks.cpp.obj"
+  "D:/Progect/C++/Tetris/main.cpp" "D:/Progect/C++/Tetris/cmake-build-debug/CMakeFiles/Tetris.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/Source/Block.cpp.obj"
+  "CMakeFiles/Tetris.dir/Source/Blocks.cpp.obj"
+  "CMakeFiles/Tetris.dir/Source/Board.cpp.obj"
+  "CMakeFiles/Tetris.dir/Source/OBlocks.cpp.obj"
+  "CMakeFiles/Tetris.dir/main.cpp.obj"
+  "Tetris.exe"
+  "Tetris.exe.manifest"
+  "Tetris.pdb"
+  "libTetris.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
